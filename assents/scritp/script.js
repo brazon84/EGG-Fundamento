bdf06f8c832179f63a78bfc633de444832df938e
Jose Gregorio Brazon Lopez <94471674+brazon84@users.noreply.github.com>
2023-05-17 23:13:33 -0300

    mejoras de alert

--- assents/scritp/script.js
+++ assents/scritp/script.js
@@ -7,32 +7,35 @@ function showAlert() {
   swal("¡hola, esta es una alerta desde JavaScript");
 }
 
-document.getElementById("title").innerHTML="Bienvenidos a mi primer Website!"
+document.getElementById("title").innerHTML = "Bienvenidos a mi primer Website!";
 
-
-document.querySelector("button.button-menu-toggle")
-    .addEventListener("click", function() {
-           document.querySelector(".nav-links").
-                      classList.toggle("nav-links-responsive")})
+document
+  .querySelector("button.button-menu-toggle")
+  .addEventListener("click", function () {
+    document
+      .querySelector(".nav-links")
+      .classList.toggle("nav-links-responsive");
+  });
 
 //se crea la funcion sumi
-const form = document.getElementById('myForm');
-form.addEventListener('submit', function(event) {
+const form = document.getElementById("myForm");
+form.addEventListener("submit", function (event) {
   event.preventDefault();
   validateForm();
 });
 function validateEmail(email) {
-  const regex =  /^[^\s@]+@[^\s@]+\.[^\s@]{2,7}$/
-  return regex.test(email)
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,7}$/;
+  return regex.test(email);
 }
 function validateForm() {
-  const emailInput = document.getElementById('email');
+  const emailInput = document.getElementById("email");
   const email = emailInput.value;
   if (!validateEmail(email)) {
-    swal('Por favor ingrese un correo electrónico válido.');
+    swal("Por favor ingrese un correo electrónico válido.");
   } else {
-    swal({title:'Correo electrónico enviado correctamente.',
-    icon:'success',
-  });
+    swal({
+      title: "Correo electrónico enviado correctamente.",
+      icon: "success",
+    });
   }
 }
