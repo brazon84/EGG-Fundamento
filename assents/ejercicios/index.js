68bc8829db693037e9ef30b642ec93e83dbff0aa
Jose Gregorio Brazon Lopez <94471674+brazon84@users.noreply.github.com>
2023-05-17 22:49:58 -0300

    l

---
+++ assents/ejercicios/index.js
@@ -0,0 +1,7 @@
+// encuentra al elemento HTML (el boton) mediante su id
+const miBoton = document.getElementById('miBoton');
+
+//Define la funcion que se ejecutara cuando se haga clic en el boton
+function handleClick(){
+    alert('¡Hola! Has hecho click en el boton');
+}
---
+++ script.js
@@ -0,0 +1,36 @@
+/*const saludar = document.getElementById("title");
+const hola = "Bienvenidos a mi primer Website!";
+saludar.innerHTML = hola;
+document.body.appendChild(saludar);
+*/
+function showAlert() {
+  alert("¡hola, esta es una alerta desde JavaScript");
+}
+
+document.getElementById("title").innerHTML="Bienvenidos a mi primer Website!"
+
+
+document.querySelector("button.button-menu-toggle")
+    .addEventListener("click", function() {
+           document.querySelector(".nav-links").
+                      classList.toggle("nav-links-responsive")})
+
+//se crea la funcion sumi
+const form = document.getElementById('myForm');
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  validateForm();
+});
+function validateEmail(email) {
+  const regex =  /^[^\s@]+@[^\s@]+\.[^\s@]{2,7}$/
+  return regex.test(email)
+}
+function validateForm() {
+  const emailInput = document.getElementById('email');
+  const email = emailInput.value;
+  if (!validateEmail(email)) {
+    alert('Por favor ingrese un correo electrónico válido.');
+  } else {
+    alert('Correo electrónico enviado correctamente.');
+  }
+}
